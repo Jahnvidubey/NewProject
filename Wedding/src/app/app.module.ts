b40bc92c3d0521d53bb2403f07f6185a2d0d8e29
jahnvi dubey <jahnvi.dubey10@gmail.com>
2022-12-31 13:36:25 +0530

    initial commit

--- Wedding/src/app/app.module.ts
+++ Wedding/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule
