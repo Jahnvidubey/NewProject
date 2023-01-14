a4b617c43a907e7ba3fc1fa6f14687387809c556
jahnvi dubey <jahnvi.dubey10@gmail.com>
2023-01-14 12:38:59 +0530

    commit

--- Wedding/src/app/app.module.ts
+++ Wedding/src/app/app.module.ts
@@ -1,48 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from 'src/environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatInputModule} from '@angular/material/input';
-import { MatSelectModule} from '@angular/material/select';
-import { MatIconModule} from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = []
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ForgetPasswordComponent,
-    SignInComponent,
-    SignUpComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AppRoutingModule,
-    FormsModule,
-    MatSlideToggleModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
-    ReactiveFormsModule,
-    MatRadioModule,
-    MatButtonModule,
-    MatDatepickerModule
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
