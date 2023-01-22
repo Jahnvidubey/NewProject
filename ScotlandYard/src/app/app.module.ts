53ae92c4393e34996bf7139b6098a10df768ca6b
jahnvi dubey <jahnvi.dubey10@gmail.com>
2023-01-22 13:13:14 +0530

    half done

--- ScotlandYard/src/app/app.module.ts
+++ ScotlandYard/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-
 @NgModule({
   declarations: [
     AppComponent,
